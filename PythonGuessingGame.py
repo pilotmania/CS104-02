387d1abb2b0637603e0505e1118a9416330c15e1
Vincent Fossetta <s1288702@monmouth.edu>
2019-09-24 14:21:59 -0400

    PythonGuessingGame

--- PythonGuessingGame.py
+++ PythonGuessingGame.py
@@ -9,17 +9,24 @@ gameOver = False
 while gameOver != True:
     guess = int(input ("Enter a guess: "))
     if guess == theComputerNumber:
-        print ("You win! You guess the right number!")
-        gameOver = True
-    if guess != theComputerNumber:
-        print ("incorrect, keep guessing")
-        numberOfGuesses = numberOfGuesses +1
-        print ("Number of guesses:{}.".format(numberOfGuesses))
-        gameOver = False
-    if numberOfGuesses == 20:
-        print ("Game Over! You ran out of guesses!")
+        print ("You win! You guessed the right number!")
         gameOver = True
     if guess < lowGuessRange:
         print ("Your guess is too low!")
     if guess > highGuessRange:
         print ("Your guess is too high!")
+    if guess != theComputerNumber:
+        print ("Incorrect, keep guessing!")
+        numberOfGuesses = numberOfGuesses +1
+        print ("Number of guesses:{}.".format(numberOfGuesses))
+        gameOver = False
+    if guess < theComputerNumber:
+        highGuessRange == guess
+        print ("The guess is lower than the number!")
+    if guess > theComputerNumber:
+        lowGuessRange == guess
+        print ("The guess is higher than the number!")
+    if numberOfGuesses == 20:
+        print ("Game Over! You ran out of guesses!")
+        gameOver = True
+    
